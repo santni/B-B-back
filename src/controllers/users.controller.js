0ab55ad856b1d33250e05dbeeb3088a9d141c857
Pedro Monteiro <pedromonteiro.senai564@gmail.com>
2024-05-15 11:34:23 -0300

    feat: finish users CRUD

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,4 +1,5 @@
 const pool = require('../config/database.config');
+const { verifyEmail, verifyCpf, verifyPassword } = require('../models/verify.functions');
 
 const getAllUsers = async(req, res) => {
     try {
@@ -14,12 +15,64 @@ const getAllUsers = async(req, res) => {
 
 const postUser = async(req, res) => {
     try {
-        const { name, email, cpf, telephone, address } = req.params;
-        
+        const { name, email, cpf, telephone, password, address } = req.body;
+
+        if(name.length < 10) {
+            return res.status(400).send({ message: 'short_name' });
+        } else if(!verifyEmail(email)) {
+            return res.status(400).send({ message: 'invalid_email' });
+        } else if(!verifyCpf(cpf)) {
+            return res.status(400).send({ message: 'invalid_cpf' });
+        } else if(telephone.length < 11) {
+            return res.status(400).send({ message: 'invalid_telephone' });
+        } else if(!verifyPassword(password)) {
+            return res.status(400).send({ message: 'invalid_password' });
+        } else {
+            await pool.query('INSERT INTO users(name, email, cpf, telephone, password, address) VALUES($1, $2, $3, $4, $5, $6);',
+        [name, email, cpf, telephone, password, address]);
+            return res.status(201).send({ message: 'user successfully registered' });
+        }
     } catch(e) {
         console.log('Could not POST user, server error', e);
         return res.status(500).send({ message: 'Could not HTTP POST' });
     }
 }
 
-module.exports = { getAllUsers };
+const putUser = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, email, cpf, telephone, password, address } = req.body;
+        
+        if(name.length < 10) {
+            return res.status(400).send({ message: 'short_name' });
+        } else if(!verifyEmail(email)) {
+            return res.status(400).send({ message: 'invalid_email' });
+        } else if(!verifyCpf(cpf)) {
+            return res.status(400).send({ message: 'invalid_cpf' });
+        } else if(telephone.length < 11) {
+            return res.status(400).send({ message: 'invalid_telephone' });
+        } else if(!verifyPassword(password)) {
+            return res.status(400).send({ message: 'invalid_password' });
+        } else {
+            await pool.query('UPDATE users SET name=$1, email=$2, cpf=$3, telephone=$4, password=$5, address=$6 WHERE id=$7;',
+        [name, email, cpf, telephone, password, address, id]);
+            return res.status(200).send({ message: 'user successfully updated' });
+        }
+    } catch(e) {
+        console.log('Could not PUT user, server error', e);
+        return res.status(500).send({ message: 'Could not HTTP PUT' });
+    }
+}
+
+const deleteUser = async(req, res) => {
+    try {
+        const { id } = req.params;
+        await pool.query('DELETE FROM users WHERE id=$1', [id]);
+        return res.status(200).send({ message: 'user successfully deleted' });
+    } catch(e) {
+        console.log('Could not DELETE user, server error', e);
+        return res.status(500).send({ message: 'Could not HTTP DELETE' });
+    }
+}
+
+module.exports = { getAllUsers, postUser, putUser, deleteUser };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const usersController = require('../controllers/users.controller');
 
 router.get('/', usersController.getAllUsers);
+router.post('/', usersController.postUser);
+router.put('/:id', usersController.putUser);
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
