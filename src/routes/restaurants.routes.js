0f0cdac8c3081cec0d3b2da76e32b0feab75cec0
Felipe Miotto <felipemiotto.564@gmail.com>
2024-05-20 09:40:38 -0300

    controller and routes restaurants

---
+++ src/routes/restaurants.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const restaurantsController = require('../controllers/restaurants.controller')
+
+router.get('/', restaurantsController.getRestaurants);
+router.get('/:nome', restaurantsController.getRestaurantsByName);
+router.get('/:id', restaurantsController.getRestaurantsById);
+router.post('/', restaurantsController.postRestaurants);
+router.put('/', restaurantsController.postRestaurants);
+router.delete('/:id', restaurantsController.deleteRestaurantsById);
+
+module.exports = router;
