ce8552ea2e4e1d702bb40891b372221e996da2ed
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-06-04 21:24:48 -0300

    feat: products controller

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -39,16 +39,17 @@ const postController = async(req, res) => {
         const { name, description, price, restaurantid } = req.body;
 
         if(!name || !description || !price || !restaurantid) {
-            res.status(200).send({ message: 'complete all data' });
+            res.status(400).send({ message: 'complete all data' });
         } else if(name.length < 3) {
-            res.status(200).send({ message: 'short name' });
+            res.status(400).send({ message: 'short name' });
         } else if(description.length < 10) {
-            res.status(200).send({ message: 'short description' });
+            res.status(400).send({ message: 'short description' });
         } else if(typeof price !== 'number' || typeof restaurantid !== 'number') {
-            res.status(200).send({ message: 'type is not a number' });
+            res.status(400).send({ message: 'type is not a number' });
         } else {
             await pool.query('INSERT INTO products(name, description, price, restaurantid) VALUES($1, $2, $3, $4)',
             [name, description, price, restaurantid]);
+            res.status(201).send({ message: 'product created' });
         }
         
     } catch(e) {
@@ -57,4 +58,54 @@ const postController = async(req, res) => {
     }
 }
 
-module.exports = { getAllProducts, getProductById, postController };
+const putController = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, description, price, restaurantid } = req.body;
+
+        const product = (await pool.query('SELECT * FROM products WHERE id = $1')).rows;
+
+        if(!product) {
+            res.status(404).send({ message: 'this product doesnt exist' });
+        }
+
+        if(!name || !description || !price || !restaurantid) {
+            res.status(400).send({ message: 'complete all data' });
+        } else if(name.length < 3) {
+            res.status(400).send({ message: 'short name' });
+        } else if(description.length < 10) {
+            res.status(400).send({ message: 'short description' });
+        } else if(typeof price !== 'number' || typeof restaurantid !== 'number') {
+            res.status(400).send({ message: 'type is not a number' });
+        } else {
+            await pool.query('UPDATE products SET name=$1, description=$2, price=$3, restaurant=$4 WHERE id=$5',
+            [name, description, price, restaurantid, id]);
+            res.status(200).send({ message: 'product updated' });
+        }
+
+    } catch(e) {
+        console.log('Could not PUT HTTP method');
+        return res.status(505).send({ message: 'Error in PUT' }); 
+    }
+}
+
+const deleteController = async(req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = (await pool.query('SELECT * FROM products WHERE id = $1')).rows;
+
+        if(!product) {
+            res.status(404).send({ message: 'this product doesnt exist' });
+        }
+
+        await pool.query('DELETE FROM products WHERE id=$1', [id]);
+        res.status(200).send({ message: 'product deleted' });
+
+    } catch(e) {
+        console.log('Could not PUT HTTP method');
+        return res.status(505).send({ message: 'Error in PUT' }); 
+    }
+}
+
+module.exports = { getAllProducts, getProductById, postController, putController, deleteController };
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,5 +4,8 @@ const productsControllers = require('../controllers/products.controller');
 
 router.get('/:id', productsControllers.getAllProducts);
 router.get('/product/:id', productsControllers.getProductById);
+router.post('/', productsControllers.postController);
+router.put('/product/:id', productsControllers.putController);
+router.delete('/product/:id', productsControllers.deleteController)
 
 module.exports = { router };
