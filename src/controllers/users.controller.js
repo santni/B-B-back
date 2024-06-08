702a63db660ded75f44a923d53454878d344a7f8
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-06-08 00:35:35 -0300

    fix: remove libs & delete users functions

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,9 +1,5 @@
 const pool = require('../config/database.config');
 const { verifyEmail, verifyCpf, verifyPassword } = require('../models/verify.functions');
-const dayjs = require('dayjs');
-const hash = require('bcrypt');
-const { sign } = require('jsonwebtoken');
-const crypto = require('crypto');
 
 const getAllUsers = async(req, res) => {
     try {
@@ -118,76 +114,5 @@ const deleteUser = async(req, res) => {
         return res.status(500).send({ message: 'Could not HTTP DELETE' });
     }
 }
-
-const loginUser = async(req, res) => {
-    try {
-        const { email, password } = req.body;
-        const getUser = (await pool.query('SELECT * FROM users WHERE email=$1', [email])).rows;
-
-        if(getUser.length == 0) {
-            return res.status(500).send({ message: 'User not found' });
-        }
-
-        const token = sign({}, "d2093a95-ed12-46fb-9bb4-8c16d28e6013", {
-            subject: userId,
-            expiresIn: "5m",
-          });
-      
-          // expirar o login depois de 14 dias e ser obrigado a logar novamente
-          const expiresIn = dayjs().add(14, "day").unix();
-          const generateToken = () => {
-            return crypto.randomBytes(15).toString("hex");
-          };
-          const generatertoken = generateToken();
-          const generateRefreshToken = await pool.query(
-            "INSERT INTO rtoken (rtoken, expires, user_id) VALUES ($1, $2, $3) RETURNING *",
-            [generatertoken, expiresIn, email]
-          );
-      
-          return res.status(200).send({
-            token,
-            refreshToken: generateRefreshToken.rows[0].rtoken,
-            user: user
-          });
-
-    } catch(e) {
-        console.log('Could not GET user, server error', e);
-        return res.status(500).send({ message: 'Could not HTTP GET' });
-    }
-}
-
-async function refreshToken(req, res) {
-    try {
-      const { refreshToken } = req.body;
-  
-      const token = await pool.query('SELECT * FROM rtoken WHERE rtoken = $1', [refreshToken]);
-  
-      if (!token.rows[0]) {
-        return res.status(404).send({ message: "Token inválido" });
-      }
-  
-      const newToken = sign({}, 'ao6Al-mR50ruM4-vq231Vs-gO418uibMe', {
-        subject: token.rows[0].token.toString(),
-        expiresIn: '15m'
-      });
-  
-      return res.status(200).send({ token: newToken, refreshToken: token.rows[0] });
-    } catch (error) {
-      return res.status(500).send({ message: "Erro ao realizar refresh", error: error.message });
-    }
-  };
-
-  async function logOut (req, res) {
-    try {
-      const { refreshToken } = req.body;
-  
-      await pool.query('DELETE FROM rtoken WHERE rtoken = $1', [refreshToken]);
-  
-      return res.status(200).send({ message: "Usuário deslogado com sucesso" });
-    }
-    catch (error) {
-      return res.status(500).send({ message: "Erro ao deslogar", error: error.message });
-    }
-  }
 
 module.exports = { getAllUsers, getUserByEmail, getUserByCPF, postUser, putUser, deleteUser };
